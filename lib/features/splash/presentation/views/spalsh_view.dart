b547585fb7978e3f50dae8754fb0d74aca6960ac
tamemmohamed <tamemmohamed35@gmail.com>
2024-10-03 14:10:37 +0300

    first commit

---
+++ lib/features/splash/presentation/views/spalsh_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SpalshView extends StatelessWidget {
+  const SpalshView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
