c78cc3c1f4882e5c53546f8822ff1c895621684c
tamemmohamed <tamemmohamed35@gmail.com>
2024-10-14 20:26:59 +0300

    used cached network image

--- lib/features/home/presentation/views/widgets/custom_bokk_image_item.dart
+++ lib/features/home/presentation/views/widgets/custom_bokk_image_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImageItem extends StatelessWidget {
@@ -7,17 +8,20 @@ class CustomBookImageItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(
-            16,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(
+        16,
+      ),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(),
           ),
-          image: DecorationImage(
-            image: NetworkImage(
-              imageUrl,
-            ),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
           ),
         ),
       ),
--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -14,14 +14,14 @@ class FeaturedBooksListView extends StatelessWidget {
       builder: (context, state) {
         if (state is FeaturdBooksSuccess) {
           return SizedBox(
-            height: MediaQuery.of(context).size.height * .3,
+            height: MediaQuery.of(context).size.height * .25,
             child: ListView.builder(
               physics: const BouncingScrollPhysics(),
               itemCount: state.books.length,
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
                 return Padding(
-                  padding: EdgeInsets.only(
+                  padding: const EdgeInsets.only(
                     right: 8,
                   ),
                   child: CustomBookImageItem(
